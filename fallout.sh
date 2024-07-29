04fecc8095d66b303db60fdc6c550c89b7aeb844
overkillwtf <desk@overkill.wtf>
2024-07-30 00:58:42 +0200

    Update fallout.sh

--- fallout.sh
+++ fallout.sh
@@ -3,12 +3,23 @@
 echo "Fallout London Patcher by Timo Schmidt and Kevin Wammer for overkill.wtf"
 sleep 1
 
+# Paths
+STEAM_APPMANIFEST_PATH="$HOME/.local/share/Steam/steamapps/appmanifest_377160.acf"
+NONSTEAM_LAUNCHERS_DIR="$HOME/.local/share/Steam/steamapps/compatdata/NonSteamLaunchers/pfx/drive_c/Program Files (x86)/GOG Galaxy/"
+DOWNLOADS_DEPOTS_DIR="$HOME/Downloads/Depots"
+DOWNGRADE_LIST_PATH="$HOME/Downloads/folon_downgrade.txt"
+STEAMCMD_DIR="$HOME/Downloads/SteamCMD"
+FALLOUT_LONDON_DIR="$HOME/.local/share/Steam/steamapps/compatdata/NonSteamLaunchers/pfx/drive_c/Program Files (x86)/GOG Galaxy/Games/Fallout London"
+FALLOUT_4_DIR="$HOME/.steam/steam/steamapps/common/Fallout 4"
+FALLOUT4_CONFIG_DIR="$HOME/.local/share/Steam/steamapps/compatdata/377160/pfx/drive_c/users/steamuser/Documents/My Games/Fallout4"
+FALLOUT4_APPDATA_DIR="$HOME/.local/share/Steam/steamapps/compatdata/377160/pfx/drive_c/users/steamuser/AppData/Local/Fallout4"
+F4LONDON_INI_URL="https://github.com/krupar101/f4london_steam_deck_ini/blob/main/Fallout4.INI"
+
 # Set appmanifest_377160.acf to read-only
 echo "Setting appmanifest_377160.acf to read-only to stop Steam from updating the game..."
-chmod 444 "$HOME/.local/share/Steam/steamapps/appmanifest_377160.acf"
+chmod 444 "$STEAM_APPMANIFEST_PATH"
 
 # Check if NonSteamLaunchers is already installed
-NONSTEAM_LAUNCHERS_DIR="$HOME/.local/share/Steam/steamapps/compatdata/NonSteamLaunchers/pfx/drive_c/Program Files (x86)/GOG Galaxy/"
 if [ -e "$NONSTEAM_LAUNCHERS_DIR" ]; then
     echo "NonSteamLaunchers is already installed."
 else
@@ -18,11 +29,11 @@ fi
 
 # Create necessary directories
 echo "Creating necessary directories..."
-mkdir -p "$HOME/Downloads/Depots"
+mkdir -p "$DOWNLOADS_DEPOTS_DIR"
 
 # Setting up downgrade-list
 echo "Setting up downgrade-list..."
-cat <<EOL > "$HOME/Downloads/folon_downgrade.txt"
+cat <<EOL > "$DOWNGRADE_LIST_PATH"
 download_depot 377160 377161 7497069378349273908
 download_depot 377160 377163 5819088023757897745
 download_depot 377160 377162 5847529232406005096
@@ -45,8 +56,8 @@ sleep 1
 
 # Setting up SteamCMD
 echo "Setting up SteamCMD..."
-mkdir -p "$HOME/Downloads/SteamCMD"
-cd "$HOME/Downloads/SteamCMD"
+mkdir -p "$STEAMCMD_DIR"
+cd "$STEAMCMD_DIR" || { echo "Failed to change directory to $STEAMCMD_DIR"; exit 1; }
 curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
 
 sleep 1
@@ -60,72 +71,72 @@ password=$(zenity --password --title="Steam Password" --text="Enter your Steam u
 
 # Run SteamCMD with the provided credentials and script
 echo "Running SteamCMD with provided credentials..."
-chmod +x "$HOME/Downloads/SteamCMD/steamcmd.sh"
-"$HOME/Downloads/SteamCMD/steamcmd.sh" +login "$username" "$password" +runscript "$HOME/Downloads/folon_downgrade.txt"
+chmod +x "$STEAMCMD_DIR/steamcmd.sh"
+"$STEAMCMD_DIR/steamcmd.sh" +login "$username" "$password" +runscript "$DOWNGRADE_LIST_PATH"
 
 # Move downloaded content and clean up
 echo "Moving downloaded content and cleaning up..."
-rsync -a "$HOME/Downloads/SteamCMD/linux32/steamapps/content/app_377160/"* "$HOME/Downloads/Depots/"
-rm -rf "$HOME/Downloads/SteamCMD"
-rm "$HOME/Downloads/folon_downgrade.txt"
+rsync -a "$STEAMCMD_DIR/linux32/steamapps/content/app_377160/"* "$DOWNLOADS_DEPOTS_DIR/"
+rm -rf "$STEAMCMD_DIR"
+rm "$DOWNGRADE_LIST_PATH"
 
 echo "Patch process completed successfully!"
 
 # Check for Fallout: London installation
-if [ ! -d "$HOME/.local/share/Steam/steamapps/compatdata/NonSteamLaunchers/pfx/drive_c/Program Files (x86)/GOG Galaxy/Games/Fallout London" ]; then
-  text="`printf "<b>Please download Fallout: London from GOG and install it!</b>\n\nThen click OK to continue. (You can find GOG in your Steam Library.)"`"
-   zenity --info \
-           --title="Overkill" \
-           --width="450" \
-           --text="${text}" 2>/dev/null
+if [ ! -d "$FALLOUT_LONDON_DIR" ]; then
+  text="<b>Please download Fallout: London from GOG and install it!</b>\n\nThen click OK to continue. (You can find GOG in your Steam Library.)"
+  zenity --info \
+         --title="Overkill" \
+         --width="450" \
+         --text="$text" 2>/dev/null
 fi
 
 # Step 1: Move main game files
 echo "Step 1: Moving main game files..."
-if [ -d "$HOME/.local/share/Steam/steamapps/compatdata/NonSteamLaunchers/pfx/drive_c/Program Files (x86)/GOG Galaxy/Games/Fallout London" ]; then
-    rsync -a "$HOME/.local/share/Steam/steamapps/compatdata/NonSteamLaunchers/pfx/drive_c/Program Files (x86)/GOG Galaxy/Games/Fallout London/"* "$HOME/.steam/steam/steamapps/common/Fallout 4/"
+if [ -e "$FALLOUT_LONDON_DIR" ]; then
+    rsync -a "$FALLOUT_LONDON_DIR/"* "$FALLOUT_4_DIR/"
 else
     echo "Directory for main game files not found."
 fi
 
 # Step 2: Move _config files
 echo "Step 2: Moving _config files..."
-mkdir -p "$HOME/.local/share/Steam/steamapps/compatdata/377160/pfx/drive_c/users/steamuser/Documents/My Games/Fallout4"
-if [ -d "$HOME/.local/share/Steam/steamapps/compatdata/NonSteamLaunchers/pfx/drive_c/Program Files (x86)/GOG Galaxy/Games/Fallout London/__Config" ]; then
-    rsync -a "$HOME/.local/share/Steam/steamapps/compatdata/NonSteamLaunchers/pfx/drive_c/Program Files (x86)/GOG Galaxy/Games/Fallout London/__Config/"* "$HOME/.local/share/Steam/steamapps/compatdata/377160/pfx/drive_c/users/steamuser/Documents/My Games/Fallout4/"
+mkdir -p "$FALLOUT4_CONFIG_DIR"
+if [ -e "$FALLOUT_LONDON_DIR/__Config" ]; then
+    rsync -a "$FALLOUT_LONDON_DIR/__Config/"* "$FALLOUT4_CONFIG_DIR/"
 else
     echo "__Config directory not found."
 fi
 
 # Step 3: Move _appdata files
 echo "Step 3: Moving _appdata files..."
-mkdir -p "$HOME/.local/share/Steam/steamapps/compatdata/377160/pfx/drive_c/users/steamuser/AppData/Local/Fallout4"
-if [ -d "$HOME/.local/share/Steam/steamapps/compatdata/NonSteamLaunchers/pfx/drive_c/Program Files (x86)/GOG Galaxy/Games/Fallout London/__AppData" ]; then
-    rsync -a "$HOME/.local/share/Steam/steamapps/compatdata/NonSteamLaunchers/pfx/drive_c/Program Files (x86)/GOG Galaxy/Games/Fallout London/__AppData/"* "$HOME/.local/share/Steam/steamapps/compatdata/377160/pfx/drive_c/users/steamuser/AppData/Local/Fallout4/"
+mkdir -p "$FALLOUT4_APPDATA_DIR"
+if [ -e "$FALLOUT_LONDON_DIR/__AppData" ]; then
+    rsync -a "$FALLOUT_LONDON_DIR/__AppData/"* "$FALLOUT4_APPDATA_DIR/"
 else
     echo "__AppData directory not found."
 fi
 
 # Step 4: Download and place Fallout4.INI
 echo "Step 4: Downloading and placing Fallout4.INI..."
-curl -L -o "$HOME/.local/share/Steam/steamapps/compatdata/377160/pfx/drive_c/users/steamuser/Documents/My Games/Fallout4/Fallout4.INI" https://github.com/krupar101/f4london_steam_deck_ini/blob/main/Fallout4.INI
+curl -L -o "$FALLOUT4_CONFIG_DIR/Fallout4.INI" "$F4LONDON_INI_URL"
 
 # Step 5: Renaming executables
 echo "Step 5: Renaming executables..."
-if [ -e "$HOME/.steam/steam/steamapps/common/Fallout 4/f4se_loader.exe" ]; then
-    mv -f "$HOME/.steam/steam/steamapps/common/Fallout 4/Fallout4Launcher.exe" "$HOME/.steam/steam/steamapps/common/Fallout 4/F04LauncherBackup.exe"
-    mv -f "$HOME/.steam/steam/steamapps/common/Fallout 4/f4se_loader.exe" "$HOME/.steam/steam/steamapps/common/Fallout 4/Fallout4Launcher.exe"
+if [ -e "$FALLOUT_4_DIR/f4se_loader.exe" ]; then
+    mv -f "$FALLOUT_4_DIR/Fallout4Launcher.exe" "$FALLOUT_4_DIR/F04LauncherBackup.exe"
+    mv -f "$FALLOUT_4_DIR/f4se_loader.exe" "$FALLOUT_4_DIR/Fallout4Launcher.exe"
 else
     echo "f4se_loader.exe not found."
 fi
 
 # Cleanup: Remove Fallout London directory
 echo "Cleaning up..."
-rm -rf "$HOME/.local/share/Steam/steamapps/compatdata/NonSteamLaunchers/pfx/drive_c/Program Files (x86)/GOG Galaxy/Games/Fallout London"
+rm -rf "$FALLOUT_LONDON_DIR"
 
-text="`printf "<b>All steps completed successfully!</b>\n\nYou can now close the terminal / Konsole."`"
+text="<b>All steps completed successfully!</b>\n\nYou can now close the terminal / Konsole."
 zenity --info \
        --title="Overkill" \
        --width="450" \
-       --text="${text}" 2>/dev/null
+       --text="$text" 2>/dev/null
 exit
