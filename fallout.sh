9d2b438dd69b4ddb12b67b32efcb6f7baa950983
krupar101 <61988573+krupar101@users.noreply.github.com>
2024-08-08 02:11:37 +0200

    Update fallout.sh

--- fallout.sh
+++ fallout.sh
@@ -418,7 +418,13 @@ if [ "$LAST_STEP" -lt 7 ]; then
         zenity --info --title="Manual Installation" --width="450" --text="GoG installer for Fallout London will now launch.\n1. Click Install\n2. Select Drive D:\n3. Click Install Here\n\nClose the installer after it's done to continue the setup process.\n\nClick 'OK' in this window to start the process." 2>/dev/null
 
 	printf "\n\nGoG installer for Fallout London will now launch.\n\n1. Click Install\n2. Select Drive D:\n3. Click Install Here\n\nClose the installer after it's done to continue the setup process.\n\n"
-	
+
+	# Print environment variables for verification
+	echo "STEAM_COMPAT_CLIENT_INSTALL_PATH: $STEAM_COMPAT_CLIENT_INSTALL_PATH"
+	echo "WINEPREFIX: $WINEPREFIX"
+	echo "PROTON_DIR: $PROTON_DIR"
+	echo "GAME_EXE_PATH: $GAME_EXE_PATH"
+ 
 	# Run the game using Proton with the specified Wine prefix and compatibility data path
 	STEAM_COMPAT_CLIENT_INSTALL_PATH="$STEAM_COMPAT_CLIENT_INSTALL_PATH" WINEPREFIX="$WINEPREFIX" "$PROTON_DIR/proton" run "$GAME_EXE_PATH"
 
