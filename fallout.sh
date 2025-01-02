78a945fe71af38d72e3ecfe9c24fb68c1302ae22
JimDoubleW <wincent.voisin@gmail.com>
2025-01-02 20:15:19 +0000

    Update fallout.sh

--- fallout.sh
+++ fallout.sh
@@ -683,7 +683,7 @@ if [ "$LAST_STEP" -lt 7 ]; then
 
 		echo "replace FALLOUT_LONDON_DIR=$FALLOUT_LONDON_DIR by:"
   		FALLOUT_LONDON_DIR="$WINEPREFIX/dosdevices/i:"
-    		FALLOUT_LONDON_DIR="$WINEPREFIX/dosdevices/z:/home/user/Games/Heroic/Fallout London"
+    		FALLOUT_LONDON_DIR="/home/user/Games/Heroic/Fallout London"
 		export FALLOUT_LONDON_DIR
   
       		echo "FALLOUT_4_DIR=$FALLOUT_4_DIR"
