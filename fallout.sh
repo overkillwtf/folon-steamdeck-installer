7d857dec90cdaddb760de7419bd6bf577316ace1
JimDoubleW <wincent.voisin@gmail.com>
2025-01-02 20:24:39 +0100

    Update

--- fallout.sh
+++ fallout.sh
@@ -678,7 +678,7 @@ if [ "$LAST_STEP" -lt 7 ]; then
 		fi
 
 		echo "replace FALLOUT_LONDON_DIR=$FALLOUT_LONDON_DIR by:"
-  		FALLOUT_LONDON_DIR="$WINEPREFIX/dosdevices/i:"
+  		export FALLOUT_LONDON_DIR="$WINEPREFIX/dosdevices/i:"
     	echo "FALLOUT_LONDON_DIR=$FALLOUT_LONDON_DIR"
 
 		# Run the game using Proton with the specified Wine prefix and compatibility data path
